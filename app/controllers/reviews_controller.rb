664199d11fd0103763f7acf5f0eadb8615dae5f3
Ankur Kothari <ankothari@gmail.com>
2015-02-12 09:40:36 +0530

    user can write a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,23 @@ class ReviewsController < ApplicationController
   def index
     @reviews = Item.find(params[:item_id]).reviews
   end
+
+  def new
+  	item = Item.find(params[:item_id])
+  	@review = item.reviews.build
+  end
+
+  def create
+  	item = Item.find(params[:item_id])
+  	review = item.reviews.create(review_params)
+  	review.user = current_user
+  	review.save
+  	redirect_to item_reviews_path(item)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :body, :stars)
+  end
 end
