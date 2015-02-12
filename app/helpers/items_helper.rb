d0e9131136f099e57ad5b360a3c7811165e21da2
Frank West <frank.west.iii@gmail.com>
2015-02-12 15:37:33 -0800

    Fixed item review code
    This does a couple of things
    1. It adds an items relationship to the order by using has_many through
    2. Nil checks for unauthenticated user
    3. Adds back in view logic to show/hide review link

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,11 +1,8 @@
 module ItemsHelper
   def bought_by item, user
-    orders = user.orders
-    line_items = orders.map { |order| order.line_item }
-    items = line_items.map { |line_item| line_item.item }
-    if items.include? self
-      return true
-    end
-    return false
+    return false unless user
+    items = user.orders.where(status: 'completed').
+      flat_map(&:items)
+    return items.include? item
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ActiveRecord::Base
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
   STATUS_TYPES = ["completed", "ordered", "cancelled", "paid"]
   has_many :line_items, dependent: :destroy
+  has_many :items, through: :line_items
   belongs_to :user
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
@@ -14,19 +15,19 @@ class Order < ActiveRecord::Base
 
   def total_amount
     sum = 0
-    line_items.each do |line_item| 
+    line_items.each do |line_item|
       sum += line_item.quantity
     end
     sum
   end
 
- #  def paypal_url(return_url) 
- #    values = { 
+ #  def paypal_url(return_url)
+ #    values = {
  #    :business => 'xxxxxxxx@xxx.com',
  #        :cmd => '_cart',
  #    :upload => 1,
  #    :return => return_url,
- #    }   values.merge!({ 
+ #    }   values.merge!({
  #    "amount_1" => unit_price,
  #    "item_name_1" => name,
  #    "item_number_1" => id,
@@ -34,5 +35,5 @@ class Order < ActiveRecord::Base
  #    })
  #         # For test transactions use this URL
  #    "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
- # end 
+ # end
 end
