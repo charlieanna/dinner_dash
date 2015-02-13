1a757b30859dede54c249ff5863bf797ee644a65
Ankur Kothari <ankothari@gmail.com>
2015-02-13 08:30:39 +0530

    spec for not able to write review passing

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -2,7 +2,7 @@ module ItemsHelper
   def bought_by item, user
     return false unless user
     items = user.orders.where(status: 'completed').
-      flat_map(&:items)
+    flat_map(&:items)
     return items.include? item
   end
 end
