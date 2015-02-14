121d5034b89e46730d4aeec9cade4fd162d56c01
Ankur Kothari <ankothari@gmail.com>
2015-02-15 02:06:37 +0530

    made payment belong to order isntead of order

--- app/models/order.rb
+++ app/models/order.rb
@@ -27,15 +27,17 @@ class Order < ActiveRecord::Base
       :cmd => '_cart',
       :upload => 1,
       :return => return_url,
-      :notify_url => notify_url
-    }  
-    values.merge!({
-      "amount_1" => 100,
-      "item_name_1" => "name",
-      "item_number_1" => 1,
-      "quantity_1" => '1',
+      :notify_url => notify_url,
       :invoice => id
-    })
+    }  
+    line_items.each_with_index do |item, index|
+      values.merge!({
+        "amount_#{index + 1}" => item.unit_price,
+        "item_name_#{index + 1}" => item.product.name,
+        "item_number_#{index + 1}" => item.product.identifier,
+        "quantity_#{index + 1}" => item.quantity
+      })
+    end
     # For test transactions use this URL
     "https://www.sandbox.paypal.com/cgi-bin/webscr?" + values.to_query
   end
