e5e2e61b884bba22f4d5e1f3f94bc84ccc7754ec
Ankur Kothari <ankothari@gmail.com>
2015-02-15 01:39:09 +0530

    added logs

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,6 +1,7 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
   def create
+  	puts params
     PaymentNotification.create!(:params => params, :cart_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id] )
     render :nothing => true
   end
